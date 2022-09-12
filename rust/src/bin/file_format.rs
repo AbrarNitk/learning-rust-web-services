c837b7479967af5e9669522be4a819b5d3691353
abrark <abrark.asahi@gmail.com>
2022-09-12 21:16:37 +0530

    added tests

--- rust/src/bin/file_format.rs
+++ rust/src/bin/file_format.rs
@@ -55,16 +55,10 @@ fn line_split(l: &str, count: usize) -> (&str, Option<&str>){
     (l, None)
 }
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let file_path = parse_from_cli("--file").unwrap();
-    println!("Formatting: {}", file_path);
-    // let current_dir = std::env::current_dir()?;
-    let file_path = std::path::Path::new(&file_path); //current_dir.join(relative_file_path);
-    let file_content = String::from_utf8(tokio::fs::read(&file_path).await?)?;
+fn format_content(content: &str) -> String {
     let mut new_content = String::new();
     let mut remaining = "".to_string();
-    for line in file_content.split("\n") {
+    for line in content.split("\n") {
         let line = if remaining.is_empty() {
             line.to_string()
         } else {
@@ -79,9 +73,17 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         new_content.push_str(&format!("{}\n", formatted_line));
         remaining = new_remaining.map(|x| x.trim_start().to_string()).unwrap_or("".to_string());
     }
+    new_content
+}
 
-    tokio::fs::write(file_path, new_content).await?;
-
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let file_path = parse_from_cli("--file").unwrap();
+    println!("Formatting: {}", file_path);
+    // let current_dir = std::env::current_dir()?;
+    let file_path = std::path::Path::new(&file_path); //current_dir.join(relative_file_path);
+    let file_content = String::from_utf8(tokio::fs::read(&file_path).await?)?;
+    tokio::fs::write(file_path, format_content(&file_content)).await?;
     // println!("{}", new_content);
     Ok(())
 }
@@ -92,6 +94,37 @@ mod tests {
     fn find_space_position() {
         assert_eq!(super::find_space_from("123 12321", 3), Some(3));
     }
+
+    #[test]
+    fn current_behavior_1() {
+        let input = r#"
+When reading the label of a beauty product, you may feel like you need a translator to figure out the laundry list of ingredients.
+
+Even products with few ingredients may still have words you’ve never heard of. You may be unable to pronounce them, let alone understand what they do.
+
+Then there’s marketing copy and social media, which speak of newly-trending ingredients that you (apparently) can’t live without. Hyaluronic acid, plant-based ceramides, and CBD are just some of the must-have ingredients that have popped up on feeds recently.
+
+Of course, you can live without any beauty product, but can some of these ingredients actually make a difference in the health of your skin?
+
+Get the scoop on what buzzwords live up to the hype and which you can skip below.
+"#;
+
+        let output = r#"
+When reading the label of a beauty product, you may feel like you need a
+translator to figure out the laundry list of ingredients.
+Even products with few ingredients may still have words you’ve never heard of.
+You may be unable to pronounce them, let alone understand what they do.
+Then there’s marketing copy and social media, which speak of newly-trending
+ingredients that you (apparently) can’t live without. Hyaluronic acid,
+plant-based ceramides, and CBD are just some of the must-have ingredients that
+have popped up on feeds recently.  Of course, you can live without any beauty
+product, but can some of these ingredients actually make a difference in the
+health of your skin?  Get the scoop on what buzzwords live up to the hype and
+which you can skip below.
+"#;
+        assert_eq!(super::format_content(input),output.to_string());
+    }
+
 }
 
 /*
