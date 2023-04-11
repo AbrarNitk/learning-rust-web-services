bd327e74ea4052e80487b28fd239343315b19fed
AbrarNitk <abrar.nitk@gmail.com>
2023-04-11 13:16:52 +0530

    changes

--- rust/src/bin/regex_example.rs
+++ rust/src/bin/regex_example.rs
@@ -1,3 +1,13 @@
+lazy_static::lazy_static! {
+    static ref PATH_PARAMS: regex::Regex = regex::Regex::new(r"<\s*([a-z]\w+)\s*:\s*([a-z|A-Z|0-9|_]\w+)\s*>").unwrap();
+}
+
+
 fn main() {
+//    let regex: regex::Regex = regex::Regex::new(r"<\s*([a-z]\w+)\s*:\s*([a-z|A-Z|0-9|_]\w+)\s*>").unwrap();
+    let text = "/books/<string:name>/<integer:a_ge>";
+    for cap in PATH_PARAMS.captures_iter(text) {
+        println!("{:?}", cap);
+    }
     println!("Hello World");
 }
